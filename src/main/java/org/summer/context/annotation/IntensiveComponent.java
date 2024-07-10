6e66cb9b827fe4529867acdd6858305b4aba7067
Lesn0y <kilril2101@gmail.com>
2024-07-10 20:54:08 +0300

    Update javadoc

--- src/main/java/org/summer/context/annotation/IntensiveComponent.java
+++ src/main/java/org/summer/context/annotation/IntensiveComponent.java
@@ -1,11 +1,15 @@
 package org.summer.context.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Annotation to indicate that a class is a component and should be managed by the IntensiveContext.
+ * Ensures that the class has a default constructor!
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface IntensiveComponent {
 }
--- src/main/java/org/summer/context/dependency/DependencyFactory.java
+++ src/main/java/org/summer/context/dependency/DependencyFactory.java
@@ -4,7 +4,19 @@ import org.summer.context.dependency.exceptions.InstanceCreationException;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * Factory class for creating instances of specified classes.
+ */
 public class DependencyFactory {
+
+    /**
+     * Creates an instance of the specified class using its default constructor.
+     *
+     * @param implClass The class object representing the type to instantiate.
+     * @return A new instance of the specified class.
+     * @throws InstanceCreationException if the instance creation fails due to instantiation,
+     *                                  IllegalAccessException, InvocationTargetException, or NoSuchMethodException.
+     */
 
     public static <T> T createInstance(Class<T> implClass) {
         try {
